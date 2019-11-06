2be659735c9e136045e9c868183d9295505df689
Philip Smith <philipm.smith@icloud.com>
2019-11-06 15:53:27 -0600

    Finished styles & Attempting JS

---
+++ js/script.js
@@ -0,0 +1,21 @@
+lightbox.option({
+    'showImageNumberLabel': false,
+    'wrapAround': true,
+    'positionFromTop': 125,
+    'alwaysShowNavOnTouchDevices': true
+})
+
+
+document.getElementById("search").addEventListener("keyup", filter);
+
+function filter() {
+  var x = document.getElementById("search");
+  x.value = x.value.toLowerCase();
+  console.log(x.value)
+}
+
+const gallery = document.getElementsByTagName("a")[0].getAttribute("data-title"); 
+
+for (let i = 0; i <= gallery.length; i++) {
+    console.log(gallery[i])
+}
