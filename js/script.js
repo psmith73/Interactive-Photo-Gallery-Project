9f6dbc2c9996eddd1fa350d4c83a4fb34bac8997
Philip Smith <philipm.smith@icloud.com>
2019-11-07 11:05:02 -0600

    Search
    Finally got the search bar

--- js/script.js
+++ js/script.js
@@ -16,27 +16,13 @@ function filter() {
     const gallery = document.querySelectorAll('a[data-title]');
     for (let i = 0; i < gallery.length; i++) {
         let caption = gallery[i].getAttribute("data-title").toLowerCase();
-        console.log("caption");
-        if(caption.indexOf("x.value") > -1) { 
-            document.getElementById("photo").style.display; 
+        
+        if(caption.indexOf(x.value)> -1) { 
+            document.getElementsByClassName('photo')[i].style.display = ""; 
         } else {
-            document.getElementById("photo").style.display = "none";
+            document.getElementsByClassName('photo')[i].style.display = "none";
         }
     }
 }
 
-// function filter() {
-//     var x = document.getElementById("search");
-//     x.value = x.value.toLowerCase();
-//     console.log(x.value);
-  
-//     const gallery = document.querySelectorAll('a[data-title]');
-//     for (let i = 0; i < gallery.length; i++) {
-//       let caption = gallery[i].getAttribute("data-title").toLowerCase();
-//       if(caption.indexOf(x.value) > -1) { 
-//         document.getElementById("photo").style.display = "none"; 
-//       } else {
-//         document.getElementById("photo").style.display;
-//       }
-//     }  
-// }
+
